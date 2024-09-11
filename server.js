63c079f83dcd369f853b8746b8e6ff7588472ee8
fbojikdfofhdfoujmvjn <shreyashv256@gmail.com>
2024-09-11 15:37:23 +0100

    added server.js

--- server.js
+++ server.js
@@ -1,21 +1,2 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
-
-// Connect to MongoDB (change the connection string as needed)
-mongoose.connect('mongodb://localhost:27017/bitewave', { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-// Basic route
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+const productRoutes = require('./routes/products');
+app.use('/api/products', productRoutes);
